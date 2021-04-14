178f8afecaeaee3290f275717b984a855293891e
李玉雷 <liyulei@ccs-MacBook-Air.local>
2021-04-14 17:11:24 +0800

    修改了test代码

--- build-stackstorm-image/file/packs/test/actions/shell/1.edit_inventory.sh
+++ build-stackstorm-image/file/packs/test/actions/shell/1.edit_inventory.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-echo	\
-"	\
-registry ansible_host=$1 ansible_port=22 ansible_user=root ansible_password=huacloud
-" \
->/home/stanley/inventory_test
+#!/usr/bin/env bash
+
+node1_1_ansible_hosts=$1
+node1_2_ansible_port=$2
+node1_3_ansible_user=$3
+node1_4_ansible_password=$4
+
+cat > /tmp/inventory_test <<- EOF
+registry ansible_host=${node1_1_ansible_hosts} ansible_port=${node1_2_ansible_port} ansible_user=${node1_3_ansible_user} ansible_password=${node1_4_ansible_password}
+EOF
+
+docker cp /tmp/inventory_test astrolabe:/etc/ansible/stage/test/inventory
+
+docker exec astrolabe cat /etc/ansible/stage/test/inventory
